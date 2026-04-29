a3ee493dc35d416962f7f7e9d12fcd9ee4bfbef3
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-29 15:49:10 -0500

    re

---
+++ src/lib/ts/routes.ts
@@ -0,0 +1,8 @@
+// ============================================
+const ROUTES = {
+  home: { path: "/", label: "HOME" },
+	profile: { path: "/profile", label: "PROFILE" },
+} as const;
+
+export default ROUTES;
+// ============================================
