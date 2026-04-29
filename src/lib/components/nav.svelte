358f214f96723a303944321b228e0bd1da4a9e4a
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-29 16:30:44 -0500

    Update nav.svelte

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -18,4 +18,24 @@
 <!-- ============================================ -->
 <!-- ============================================ -->
 
-<style></style>
+<style>
+nav {
+        position: fixed;
+        bottom: 10px;
+        left: 50%;
+        transform: translateX(-50%);
+
+        width: 500px;
+        max-width: 90vw;
+
+        display: flex;
+        justify-content: space-between;
+
+        background: rgba(128, 128, 128, 0.1);
+        backdrop-filter: blur(10px);
+
+        /*border: 1px solid gray;*/
+        border-radius: 20px;
+        padding: 10px;
+    }
+</style>
