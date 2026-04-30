92b6e5fa3ad2cc0593cc94a9fe4f3532f80a91b4
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-30 18:57:02 -0500

    re

--- src/lib/near-social-js/fun_get_profile.ts
+++ src/lib/near-social-js/fun_get_profile.ts
@@ -1,8 +1,8 @@
-import { near_kit_client } from "@near-kit-tool-box/web";
-import { near_social_client } from "./new";
-import type { Profile } from "near-social-js";
+import { near_kit_client } from '@near-kit-tool-box/web';
+import { near_social_client } from './new';
+import type { Profile } from 'near-social-js';
 // ============================================
-// Get a user's profile
-const profile: Profile | null =
-  await near_social_client(near_kit_client).getProfile(account_id);
+export async function getProfile(account_id: string): Promise<Profile | null> {
+	return near_social_client(near_kit_client).getProfile(account_id);
+}
 // ============================================
--- src/lib/near-social-js/new.ts
+++ src/lib/near-social-js/new.ts
@@ -1,7 +1,8 @@
 import { Social } from "near-social-js";
-import { Near } from "near-kit";
+// import { Near } from "near-kit";
+// leave as any for now cause of  ersion differnt but that should be Near
 // ============================================
-export function near_social_client(near: Near) {
+export function near_social_client(near: any) {
   const social = new Social({ near });
   return social;
 }
