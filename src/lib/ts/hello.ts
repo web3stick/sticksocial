30c5436b20e8fb9cbe635d52ab82c5a815643121
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-30 19:27:25 -0500

    re

---
+++ src/lib/ts/hello.ts
@@ -0,0 +1,7 @@
+import { setNetwork_mainnet } from "@near-kit-tool-box/web";
+// ========================================
+console.log("=============================");
+console.log("👋 hello this app was made \n with ❤️ by sleet.near");
+console.log("=============================");
+// ========================================
+setNetwork_mainnet();
--- src/routes/profile-auth/+page.svelte
+++ src/routes/profile-auth/+page.svelte
@@ -1,10 +1,13 @@
-<script lang="ts"></script>
+<script lang="ts">
+  import AUTH_BUTTON from "$lib/components/button_auth.svelte"
+</script>
 
 <!-- ============================================ -->
 <!-- ============================================ -->
 
 <section>
 	<h1>LOGIN</h1>
+	<AUTH_BUTTON/>
 </section>
 
 <!-- ============================================ -->
