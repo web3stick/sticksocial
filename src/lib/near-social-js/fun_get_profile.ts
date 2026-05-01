7e27e42a9ab2be1c778ee0e730b6e4186b99d03e
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-30 19:09:01 -0500

    statuic

--- src/lib/near-social-js/fun_get_profile.ts
+++ src/lib/near-social-js/fun_get_profile.ts
@@ -2,7 +2,7 @@ import { near_kit_client } from '@near-kit-tool-box/web';
 import { near_social_client } from './new';
 import type { Profile } from 'near-social-js';
 // ============================================
-export async function getProfile(account_id: string): Promise<Profile | null> {
+export async function get_profile(account_id: string): Promise<Profile | null> {
 	return near_social_client(near_kit_client).getProfile(account_id);
 }
 // ============================================
