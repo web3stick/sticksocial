fafc0cf77cbf08a6a79847e0e400c2bb6b0473b2
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-30 20:36:23 -0500

    Update +page.svelte

--- src/routes/profile/[accountId]/+page.svelte
+++ src/routes/profile/[accountId]/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import { onMount } from 'svelte';
+	import { get_profile } from '$lib/near-social-js/fun_get_profile';
+	import type { Profile } from 'near-social-js';
+	// ============================================
+	let profile = $state<Profile | null>(null);
+	// ============================================
+	onMount(async () => {
+		// =================
+		profile = await get_profile(page.params.accountId);
+		// =================
+		console.log(profile);
+		// =================
+	});
 </script>
 
 <!-- ============================================ -->
