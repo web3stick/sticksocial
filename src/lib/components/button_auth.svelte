84d1bf2fc57f8d6c082ee341b2fe1ee4a2d9b5ee
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-04-30 19:24:52 -0500

    Create button_auth.svelte

---
+++ src/lib/components/button_auth.svelte
@@ -0,0 +1,70 @@
+<!-- =========================================== -->
+<!-- =========================================== -->
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { near_connect_client } from "@near-kit-tool-box/web";
+  // ==================================
+  let isSignedIn: boolean = false;
+  let accountId: string | null = null;
+  // ==================================
+  async function updateAuthStatus() {
+    const wallet = await near_connect_client().wallet();
+    const accounts = await wallet.getAccounts();
+    if (accounts && accounts.length > 0) {
+      console.log("Connected account:", accounts[0].accountId);
+      accountId = accounts[0].accountId;
+      isSignedIn = true;
+    } else {
+      console.log("Not connected");
+      isSignedIn = false;
+    }
+  }
+  // ==================================
+  async function handleLogin() {
+    try {
+      // const wallet = await near_connect.wallet();
+      // wallet.signIn();
+      await near_connect_client().connect();
+
+      await updateAuthStatus();
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  }
+  // ==================================
+  async function handleLogout() {
+    try {
+      const wallet = await near_connect_client().wallet();
+      // near_connect.disconnect();
+      wallet.signOut();
+      await updateAuthStatus();
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  }
+  // ==================================
+  onMount(async () => {
+    await updateAuthStatus();
+  });
+</script>
+
+<!-- =========================================== -->
+<!-- =========================================== -->
+
+<!-- AUTH_BUTTON -->
+{#if isSignedIn}
+  <button on:click={handleLogout}>
+    LOGOUT {accountId}
+  </button>
+{:else}
+  <button on:click={handleLogin}> LOGIN </button>
+{/if}
+
+<!-- =========================================== -->
+<!-- =========================================== -->
+
+<style>
+  button {
+    width: 500px;
+  }
+</style>
