36138e3fb939087793ec573623dea8e73384c5e8
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-05-01 20:54:09 -0500

    Update +page.svelte

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,10 +1,13 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import BUTTON_AUTH from '$lib/components/button_auth.svelte';
+</script>
 
 <!-- ============================================ -->
 <!-- ============================================ -->
 
 <section>
 	<h1>SETTINGS</h1>
+	<BUTTON_AUTH />
 </section>
 
 <!-- ============================================ -->
