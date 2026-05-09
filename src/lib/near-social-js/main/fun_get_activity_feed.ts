b77037c55294d897c743d7d31bce9047a092f12b
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-05-08 21:18:44 -0500

    Create fun_get_activity_feed.ts

---
+++ src/lib/near-social-js/main/fun_get_activity_feed.ts
@@ -0,0 +1,13 @@
+import { near_kit_client } from '@near-kit-tool-box/web';
+import { near_social_client } from '../new';
+import type { FeedOptions } from 'near-social-js';
+// ============================================
+export async function get_activity_feed(options?: FeedOptions) {
+	const result = await near_social_client(near_kit_client).getActivityFeed(options);
+	console.log('=================');
+	console.log('/activity-feed');
+	console.log(result);
+	console.log('=================');
+	return result;
+}
+// ============================================
