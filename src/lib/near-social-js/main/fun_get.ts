a0ae5219d1f9f852b34c81a5db3fcfd3dcb19490
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-05-09 19:18:41 -0500

    Update fun_get.ts

--- src/lib/near-social-js/main/fun_get.ts
+++ src/lib/near-social-js/main/fun_get.ts
@@ -1,20 +1,17 @@
 import { near_kit_client } from "@near-kit-tool-box/web";
 import { near_social_client } from "../new";
-// ============================================
-export async function near_social_js_get_fun(keys: string[]): Promise<Record<string, any> | null> {
-	// =================
-	const result = (await near_social_client(near_kit_client()).get({ keys })) as Record<
-		string,
-		any
-	> | null;
-	// =================
-	console.log("=================");
-	console.log("get()");
-	console.log(keys.join(","));
-	console.log("=================");
-	console.log(result);
-	console.log("=================");
-	// =================
+
+export interface NearSocialGetOptions {
+	keys: string[];
+	blockHeight?: bigint;
+	returnDeleted?: boolean;
+	withBlockHeight?: boolean;
+	withNodeId?: boolean;
+	withTimestamp?: boolean;
+	useApiServer?: boolean;
+}
+
+export async function near_social_js_get_fun(options: NearSocialGetOptions): Promise<Record<string, any> | null> {
+	const result = (await near_social_client(near_kit_client()).get(options)) as Record<string, any> | null;
 	return result;
 }
-// ============================================
